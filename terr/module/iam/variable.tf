122ab0dd745821c5a29a0de7e6b61100125deb99
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-20 14:24:08 +0530

    Update variable.tf

--- terr/module/iam/variable.tf
+++ terr/module/iam/variable.tf
@@ -8,7 +8,7 @@ resource "aws_iam_user_policy_attachment" "this" {
   policy_arn = var.policy_arns[count.index]
 }
 
-[root@ip-172-31-2-146 IAM]# cat variable.tf 
+
 variable "user_name" {
   description = "IAM username"
   type        = string
