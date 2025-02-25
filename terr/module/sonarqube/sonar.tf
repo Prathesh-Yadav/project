556e8e064cd8274c3520a54fc7a8dd7186844aae
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-25 15:11:54 +0530

    Create sonar.tf

---
+++ terr/module/sonarqube/sonar.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "sonarqube" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y openjdk-17-jdk wget unzip
+
+              # Download and install SonarQube
+              sudo useradd -m -d /opt/sonarqube -s /bin/bash sonar
+              cd /opt
+              sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.3.79811.zip
+              sudo apt install -y unzip
+              sudo unzip sonarqube-9.9.3.79811.zip
+              sudo mv sonarqube-9.9.3.79811 sonarqube
+              sudo chown -R sonar:sonar /opt/sonarqube
+
+              # Create SonarQube service
+              cat <<EOT | sudo tee /etc/systemd/system/sonarqube.service
+              [Unit]
+              Description=SonarQube service
+              After=network.target
+
+              [Service]
+              Type=forking
+              User=sonar
+              Group=sonar
+              ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+              ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+              Restart=always
+
+              [Install]
+              WantedBy=multi-user.target
+              EOT
+
+              sudo systemctl daemon-reload
+              sudo systemctl enable sonarqube
+              sudo systemctl start sonarqube
+              EOF
+}
