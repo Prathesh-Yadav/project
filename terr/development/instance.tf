f2df1d6327bed1966709481ab1a44d35e97dd7c9
Prathesh98 <prathesh671998@gmail.com>
2025-02-25 14:31:34 +0530

    added instance details

--- terr/development/instance.tf
+++ terr/development/instance.tf
@@ -1,8 +1,8 @@
 module "jenkins_instance" {
   source             = "../module/instance"
-  ami               = var.ami
-  instance_type     = var.instance_type
-  key_name         = var.key_name
-  security_group_id = var.security_group_id
-  instance_name     = var.instance_name
+  ami               = ami-04b4f1a9cf54c11d0
+  instance_type     = t2.micro
+  key_name         = t
+  security_group_id = sg-046679e1377fbf5d1
+  instance_name     = jenkins_server
 }
