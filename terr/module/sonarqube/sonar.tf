7df88ec846b9e5f9e63bf75ce27afd0cdce21453
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-25 19:51:03 +0530

    Update sonar.tf

--- terr/module/sonarqube/sonar.tf
+++ terr/module/sonarqube/sonar.tf
@@ -44,6 +44,6 @@ resource "aws_instance" "sonarqube" {
 
               sudo systemctl daemon-reload
               sudo systemctl enable sonarqube
-              sudo systemctl start sonarqube
+              sudo systemctl start sonarqube            
               EOF
 }
