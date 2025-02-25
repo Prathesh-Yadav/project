960313668efa384870b3de3139539b5c7a79fd35
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-25 20:41:12 +0530

    Update variable.tf

--- terr/module/iam/variable.tf
+++ terr/module/iam/variable.tf
@@ -1,14 +1,3 @@
-resource "aws_iam_user" "this" {
-  name = var.user_name
-}
-
-resource "aws_iam_user_policy_attachment" "this" {
-  count      = length(var.policy_arns)
-  user       = aws_iam_user.this.name
-  policy_arn = var.policy_arns[count.index]
-}
-
-
 variable "user_name" {
   description = "IAM username"
   type        = string
