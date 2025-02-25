877b19f4ea28e79851aaf07e533aaaf9f5115dec
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-25 14:52:34 +0530

    Update instance.tf

--- terr/module/instance/instance.tf
+++ terr/module/instance/instance.tf
@@ -15,10 +15,13 @@ resource "aws_instance" "jenkins" {
               sudo apt install -y openjdk-17-jdk
 
               # Add Jenkins repo and install Jenkins
-              wget -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-              echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-              sudo apt update -y
-              sudo apt install -y jenkins
+              sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+                https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+              echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
+              sudo apt-get update -y
+              sudo apt-get install -y jenkins
               sudo systemctl enable jenkins
               sudo systemctl start jenkins
               EOF
