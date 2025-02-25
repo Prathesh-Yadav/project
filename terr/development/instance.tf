1a8ae1c3aa66439e79825f568a0be3401da493e7
prathesh-yadav <pratheshyadav67@gmail.com>
2025-02-25 14:15:46 +0530

    Delete terr/development/instance.tf

--- terr/development/instance.tf
+++
@@ -1,8 +0,0 @@
-module "jenkins_instance" {
-  source             = "../module/instance"
-  ami               = var.ami
-  instance_type     = var.instance_type
-  key_name         = var.key_name
-  security_group_id = var.security_group_id
-  instance_name     = var.instance_name
-}
